238cb225ae9ecc278d5391866ff1f7c1c54f1293
Elvis <1155260126@link.cuhk.edu.hk>
2026-06-24 17:43:35 +0800

    make my game working

--- script.js
+++ script.js
@@ -1,18 +1,56 @@
 const container = document.querySelector('.container');
-const sizeBtn = document.querySelector('.change-size');
-let size = 16;
-sizeBtn.addEventListener('click',()=>{
-    size = prompt('what size do you want');
+const input = document.querySelector('input');
+const label = document.querySelector('label');
+
+let isDrawing = false;
+document.addEventListener('mousedown',()=>{
+    isDrawing = true;
+})
+document.addEventListener('mouseup',()=>{
+    isDrawing = false;
 })
 
-for(let i = 0; i < size; i++){
-    for(let j = 0; j < size;j++){
-        const grid = document.createElement("div");
-        grid.classList.add("grid");
-        container.appendChild(grid);
+const createGrid = (size) =>{
 
-        grid.addEventListener('mouseover',()=>{ //didn't need to use queryselector
-            grid.classList.add("colorAdd");
-        })
+    container.innerHTML = '';
+
+    let gridSize = 640/size;
+
+    for(let i = 0; i < size; i++){
+        for(let j = 0; j < size;j++){
+            const grid = document.createElement("div");
+            grid.classList.add("grid");
+            container.appendChild(grid);
+            grid.style.width  = `${gridSize}px`;
+            grid.style.height = `${gridSize}px`;
+
+
+
+            grid.addEventListener('mouseover',()=>{ //didn't need to use queryselector
+                if(isDrawing){
+                    changeColor(grid);  
+                }
+            })
+        }
     }
 }
+
+input.addEventListener("input",()=>{
+    label.textContent=`size : ${input.value}`;
+    createGrid(input.value)
+})
+
+
+createGrid(16);
+
+
+    const changeColor = grid => {
+    const randomR = Math.round(Math.random()*256);
+    const randomG = Math.round(Math.random()*256);
+    const randomB = Math.round(Math.random()*256);
+    grid.style.backgroundColor = `rgb(${randomR},${randomG},${randomB})`;
+    }
+
+
+
+
